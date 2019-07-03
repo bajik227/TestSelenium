3bd0f33562dd169c5e13bbabcd87dda01d8d6ecf
softpath <softpath@192.168.0.7>
2019-07-03 23:21:42 +0530

    Prime numbers commit

---
+++ NewProject/src/com/testpkg/PrimeNumbers.java
@@ -0,0 +1,12 @@
+package com.testpkg;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Prime Numbers::: 2,3,5,7,11,13");
+
+	}
+
+}
